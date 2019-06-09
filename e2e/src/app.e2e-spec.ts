e385f4549b3e89bf529029cf56965527ed9ff505
Maciej Żółtowski <maciej.zoltowski@e-qsg.com>
2019-06-09 09:53:33 +0200

    e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import {AppPage} from './app.po';
+import {by, element} from 'protractor';
 
 describe('sonalake-task-angular App', () => {
     let page: AppPage;
@@ -12,7 +13,81 @@ describe('sonalake-task-angular App', () => {
         expect(page).toBeTruthy();
     });
 
-    it('should display the title', () => {
-        expect(page.getTitleText()).toEqual('Characters List');
+    describe('Star Wars List', () => {
+        it('should display the title', () => {
+            expect(page.getStarWarsListHeader()).toEqual('Characters List');
+        });
+
+        it('should has prev button', () => {
+            expect(element(by.buttonText('Previous'))).toBeTruthy();
+        });
+
+        it('should has next button', () => {
+            expect(element(by.buttonText('Next'))).toBeTruthy();
+        });
+
+        it('should start page from 1', () => {
+            const idCell = element(by.css('sl-star-wars-list tbody tr:nth-child(1) th:first-child')).getText();
+            const activePage = element(by.css('sl-star-wars-list sl-list-view-pagination li.active button')).getText();
+
+            expect(element(by.buttonText('Previous')).getAttribute('class')).toContain('disabled');
+            expect(idCell).toEqual('1');
+            expect(activePage).toContain('1');
+            expect(activePage).toContain('current');
+        });
+
+        it('should paginate be set to 10', () => {
+            const nextButton = element(by.buttonText('Next')).click();
+            const idCell = element(by.css('sl-star-wars-list tbody tr:nth-child(1) th:first-child')).getText();
+            const activePage = element(by.css('sl-star-wars-list sl-list-view-pagination li.active button')).getText();
+
+            expect(idCell).toEqual('11');
+            expect(activePage).toContain('2');
+            expect(activePage).toContain('current');
+        });
+    });
+
+    describe('Star Wars Add New', () => {
+        it('should display "add" button', () => {
+            expect(page.getCreateButton().getText()).toEqual('Add New');
+        });
+
+        it('should redirect to edit view in create mode', () => {
+            page.getCreateButton().click();
+            expect(page.getStarWarsEditHeader()).toEqual('Add Character');
+        });
+
+        describe('Creating', () => {
+            beforeEach(() => {
+                page.getCreateButton().click();
+            });
+
+            it('should not redirect to home after empty form submit', () => {
+                page.getSubmitButton().click();
+                expect(page.getStarWarsEditHeader()).toEqual('Add Character');
+            });
+        });
+    });
+
+    describe('Star Wars Edit', () => {
+        it('should display "edit" button', () => {
+            expect(page.getFirstEditButton().getText()).toEqual('Edit');
+        });
+
+        it('should redirect to edit view in edit mode', () => {
+            page.getFirstEditButton().click();
+            expect(page.getStarWarsEditHeader()).toEqual('Edit Character');
+        });
+
+        describe('Editing', () => {
+            beforeEach(() => {
+                page.getFirstEditButton().click();
+            });
+
+            it('should redirect to home after successful submit', () => {
+                page.getSubmitButton().click();
+                expect(page.getStarWarsListHeader()).toEqual('Characters List');
+            });
+        });
     });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,39 @@
 import {browser, by, element} from 'protractor';
 
 export class AppPage {
-    navigateTo() {
+    public navigateTo() {
         return browser.get('/');
     }
 
-    getTitleText() {
-        return element(by.css('h1')).getText();
+    public getStarWarsListHeader() {
+        return element(by.css('sl-star-wars-list h1')).getText();
+    }
+
+    public getStarWarsEditHeader() {
+        return element(by.css('sl-star-wars-edit h1')).getText();
+    }
+
+    public getCreateButton() {
+        return element(by.css('sl-star-wars-list button.add-new'));
+    }
+
+    public getFirstEditButton() {
+        return element(by.css('sl-star-wars-list tbody tr:nth-child(1) button.edit'));
+    }
+
+    public getFirstRemoveButton() {
+        return element(by.css('sl-star-wars-list tbody tr:nth-child(1) button.remove'));
+    }
+
+    public getFirstConfirmButton() {
+        return element(by.css('sl-star-wars-list tbody tr:nth-child(1) button.confirm'));
+    }
+
+    public getFirstCancelButton() {
+        return element(by.css('sl-star-wars-list tbody tr:nth-child(1) button.cancel'));
+    }
+
+    public getSubmitButton() {
+        return element(by.css('sl-star-wars-edit button[type=submit]'));
     }
 }
